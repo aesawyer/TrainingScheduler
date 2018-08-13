e02464ab50f973c46b80a4391c0ef620fe12d209
Adam <aesawyer@live.com>
2018-08-13 05:52:26 -0600

    Added Put capability within DynamoDB_Addition

--- DynamoDB_Addition.py
+++ DynamoDB_Addition.py
@@ -2,22 +2,29 @@ import boto3
 import os
 import json
 
+
 def lambda_handler(event, context):
-    dynamodb = boto3.client('dynamodb')
-    response = dynamodb.query(
-        TableName = 'DYNAMO_TABLE_NAME',
-        IndexName = event['pathParameters']['dateToCheck']
+    data = json.dumps(event)
+    loadedData = json.loads(data)
+    body = loadedData['body']  # string
+    loadedBody = json.loads(body)
 
-
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table(os.environ['ROSTER_TABLE_NAME'])
+    table.put_item(
+        Item={
+            'StartDate': loadedBody['StartDate'],
+            'Email': loadedBody['Email'],
+            'FirstName': loadedBody['FirstName'],
+            'LastName': loadedBody['LastName'],
+            'Certification': loadedBody['Certification']
+        }
     )
-    try:
-        enterData = dynamodb.put_item(
-            TableName = 'DYNAMO_TABLE_NAME',
-            Item = {
-                'S': event['pathParameters']['dateToCheck']
-            },
-            ConditionExpression = 'attribute_not_exists',
-        )
-    except ConditionalCheckFailedException:
-        pass
-    return "You have successfully signed up for the class on "
+    resp = {
+        "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Origin": "*",
+        },
+        "body": body
+    }
+    return resp
