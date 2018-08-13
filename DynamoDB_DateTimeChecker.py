c6d397d063cb64a39e485d24f063fa99153f3d21
Adam <aesawyer@live.com>
2018-08-13 00:51:19 -0600

    Completed DateTimeChecker.py

--- DynamoDB_DateTimeChecker.py
+++ DynamoDB_DateTimeChecker.py
@@ -1,18 +1,21 @@
 import boto3
-import json
+import os
 from boto3.dynamodb.conditions import Key, Attr
 
 def lambda_handler(event, context):
     dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table('DATES_TABLE_NAME')
+    table = dynamodb.Table(os.environ['DATES_TABLE_NAME'])
     response = table.query(
-        KeyConditionExpression=Key('Cloud Practitioner')
+        KeyConditionExpression=Key('Certification').eq('Cloud Practitioner')
     )
+
+    items = response['Items']
+
     resp = {
         "statusCode": 200,
         "headers": {
             "Access-Control-Allow-Origin": "*",
         },
-        "body": response
+        "body": items
     }
     return resp
