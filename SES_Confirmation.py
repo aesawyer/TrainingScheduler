e60461589a7b96155407afd596980bce304a5a62
Adam <aesawyer@live.com>
2018-08-21 05:56:14 -0600

    Initial SES integration

--- SES_Confirmation.py
+++ SES_Confirmation.py
@@ -3,4 +3,16 @@ import os
 import boto3
 
 def lambda_handler(event, context):
-    ses = boto3.client('ses')
+    data = json.dumps(event)
+
+    ses = boto3.client('ses', region_name=os.getenv('SES_REGION'))
+    ses.send_email(
+        Source=os.getenv('SES_EMAIL_SOURCE'),
+        Destination={'ToAddresses': data['Email']},
+        Message={
+            'Subject': {'Data': 'AWS Certification Course Confirmation'},
+            'Body': {'Data': ['Congratulations ' + data['FirstName'] + ' ' + data['LastName'] + '!/n' 
+                'You have successfully signed up for the ' + data['Certification'] + ' course starting on ' + data['StartDate'] + '.']
+            }
+        }
+    )
