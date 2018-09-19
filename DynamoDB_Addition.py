1d4a8ed6a129084f1ebca12c31654fc47bdc311a
Adam <aesawyer@live.com>
2018-09-18 18:02:05 -0600

    Optimized DynamoDB_Addition & added Architecture Diagram

--- DynamoDB_Addition.py
+++ DynamoDB_Addition.py
@@ -3,7 +3,48 @@ import os
 import json
 from boto3.dynamodb.conditions import Key
 from boto3.dynamodb.conditions import Attr
-import smtplib
+
+
+def conf_email(loadedBody):
+    ses = boto3.client('ses', os.environ['AWS_REGION'])
+    email = ses.send_email(
+        Source=os.environ['SES_EMAIL_SOURCE'],
+        Destination={
+            'ToAddresses': [
+                os.environ['SES_EMAIL_SOURCE']
+            ],
+        },
+        Message={
+            'Subject': {
+                'Data': 'New Attendee for AWS ' + loadedBody['Certification'] + ' Certification Course',
+                'Charset': 'UTF-8'
+            },
+            'Body': {
+                'Text': {
+                    'Data': loadedBody['FirstName'] + ' ' + loadedBody['LastName'] + ' has signed up for the ' +
+                            loadedBody['Certification'] + ' course on ' + loadedBody[
+                                'StartDate'] + '. An invitation should be sent to ' + loadedBody['Email'] + '.',
+                    'Charset': 'UTF-8'
+                }
+            }
+        }
+    )
+
+
+def size_check(loadedBody):
+    query = table.query(
+        KeyConditionExpression=Key('StartDate').eq(loadedBody['StartDate']),
+        FilterExpression=Attr('Certification').contains(loadedBody['Certification'])
+    )
+
+    # Removes class from ClassDates table if at or above threshhold (15)
+    if len(query['Items']) >= 15:
+        classTable.delete_item(
+            Key={
+                'Certification': loadedBody['Certification'],
+                'StartDate': loadedBody['StartDate']
+            }
+        )
 
 
 def lambda_handler(event, context):
@@ -26,42 +67,9 @@ def lambda_handler(event, context):
         }
     )
 
-    ses = boto3.client('ses', region_name=os.getenv('SES_REGION'))
-    email = ses.send_email(
-        Source=os.environ['SES_EMAIL_SOURCE'],
-        Destination={
-            'ToAddresses': [
-                os.environ['SES_EMAIL_SOURCE']
-            ],
-        },
-        Message={
-            'Subject': {
-                'Data': 'New Attendee for ' + loadedBody['Certification'] + ' Certification Training!',
-                'Charset': 'UTF-8'
-            },
-            'Body': {
-                'Text': {
-                    'Data': loadedBody['FirstName'] + ' ' + loadedBody['LastName'] + ' has signed up for the ' +
-                            loadedBody['Certification'] + ' course on ' + loadedBody['StartDate'],
-                    'Charset': 'UTF-8'
-                }
-            }
-        }
-    )
+    conf_email(loadedBody)
 
-    query = table.query(
-        KeyConditionExpression=Key('StartDate').eq(loadedBody['StartDate']),
-        FilterExpression=Attr('Certification').contains(loadedBody['Certification'])
-    )
-
-    # Removes class from ClassDates table if at or above threshhold (15)
-    if len(query['Items']) >= 15:
-        classTable.delete_item(
-            Key={
-                'Certification': loadedBody['Certification'],
-                'StartDate': loadedBody['StartDate']
-            }
-        )
+    size_check(loadedBody)
 
     resp = {
         "statusCode": 200,
